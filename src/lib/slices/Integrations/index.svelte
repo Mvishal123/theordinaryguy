d7cbdbf94a4ac54e4a3e8fc3596a91f1cd26f90e
vishal <vishalmahesh.study@gmail.com>
2024-04-22 17:21:04 +0530

    ui: add integration section

---
+++ src/lib/slices/Integrations/index.svelte
@@ -0,0 +1,67 @@
+<script>
+	import Bounded from '$lib/components/Bounded.svelte';
+	import { PrismicRichText, PrismicText } from '@prismicio/svelte';
+	import LogoBackground from './LogoBackground.svelte';
+	import StylizedLogoMark from './StylizedLogoMark.svelte';
+	import background from './integration.jpg';
+	import clsx from 'clsx';
+
+	import logoNPM from '~icons/fa6-brands/npm';
+	import logoFigma from '~icons/fa6-brands/figma';
+	import logoGithub from '~icons/fa6-brands/github';
+	import logoCloudflare from '~icons/fa6-brands/cloudflare';
+	import logoDigitalOcean from '~icons/fa6-brands/digital-ocean';
+	import logoFly from '~icons/fa6-brands/fly';
+
+	const icons = {
+		npm: logoNPM,
+		figma: logoFigma,
+		github: logoGithub,
+		cloudflare: logoCloudflare,
+		digitalocean: logoDigitalOcean,
+		fly: logoFly
+	};
+
+	/** @type {import("@prismicio/client").Content.IntegrationsSlice} */
+	export let slice;
+</script>
+
+<Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} class="overflow-hidden">
+	<img src={background} alt="background" class="absolute inset-0 h-full w-full object-cover" />
+	<LogoBackground />
+	<div class="relative">
+		<h2
+			class="mx-auto max-w-2xl bg-gradient-to-b from-violet-50 to-violet-300 bg-clip-text py-2 text-center text-5xl font-medium text-transparent md:text-7xl"
+		>
+			<PrismicText field={slice.primary.heading} />
+		</h2>
+
+		<div class="mx-auto mt-6 max-w-md text-balance text-center text-gray-300">
+			<PrismicRichText field={slice.primary.body} />
+		</div>
+
+		<div class="mt-20 flex flex-col items-center md:flex-row">
+			{#each slice.items as item, index}
+				{#if item.icon}
+					{#if Math.floor(slice.items.length) / 2 === index}
+						<StylizedLogoMark />
+						<div class="signal-line rotate-180"></div>
+					{/if}
+					<div
+						class="pulsing-icon flex aspect-square shrink-0 items-center justify-center rounded-full border border-violet-50/30 bg-violet-50/35 p-3 text-3xl text-violet-100 opacity-40 md:text-5xl"
+					>
+						<svelte:component this={icons[item.icon]} />
+					</div>
+					{#if index !== slice.items.length - 1 && item.icon}
+						<div
+							class={clsx(
+								'signal-line',
+								index >= Math.floor(slice.items.length / 2) ? 'rotate-180' : 'rotate-0'
+							)}
+						></div>
+					{/if}
+				{/if}
+			{/each}
+		</div>
+	</div>
+</Bounded>
--- src/prismicio-types.d.ts
+++ src/prismicio-types.d.ts
@@ -102,6 +102,7 @@ export type CaseStudyDocument<Lang extends string = string> = prismic.PrismicDoc
 >;
 
 type PageDocumentDataSlicesSlice =
+	| IntegrationsSlice
 	| CaseStudiesSlice
 	| ShowcaseSlice
 	| BentoSlice
@@ -531,6 +532,73 @@ type HeroSliceVariation = HeroSliceDefault;
 export type HeroSlice = prismic.SharedSlice<'hero', HeroSliceVariation>;
 
 /**
+ * Primary content in *Integrations → Primary*
+ */
+export interface IntegrationsSliceDefaultPrimary {
+	/**
+	 * Heading field in *Integrations → Primary*
+	 *
+	 * - **Field Type**: Rich Text
+	 * - **Placeholder**: *None*
+	 * - **API ID Path**: integrations.primary.heading
+	 * - **Documentation**: https://prismic.io/docs/field#rich-text-title
+	 */
+	heading: prismic.RichTextField;
+
+	/**
+	 * Body field in *Integrations → Primary*
+	 *
+	 * - **Field Type**: Rich Text
+	 * - **Placeholder**: *None*
+	 * - **API ID Path**: integrations.primary.body
+	 * - **Documentation**: https://prismic.io/docs/field#rich-text-title
+	 */
+	body: prismic.RichTextField;
+}
+
+/**
+ * Primary content in *Integrations → Items*
+ */
+export interface IntegrationsSliceDefaultItem {
+	/**
+	 * Icon field in *Integrations → Items*
+	 *
+	 * - **Field Type**: Select
+	 * - **Placeholder**: *None*
+	 * - **API ID Path**: integrations.items[].icon
+	 * - **Documentation**: https://prismic.io/docs/field#select
+	 */
+	icon: prismic.SelectField<'cloudflare' | 'github' | 'figma' | 'digitalocean' | 'npm' | 'fly'>;
+}
+
+/**
+ * Default variation for Integrations Slice
+ *
+ * - **API ID**: `default`
+ * - **Description**: Default
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type IntegrationsSliceDefault = prismic.SharedSliceVariation<
+	'default',
+	Simplify<IntegrationsSliceDefaultPrimary>,
+	Simplify<IntegrationsSliceDefaultItem>
+>;
+
+/**
+ * Slice variation for *Integrations*
+ */
+type IntegrationsSliceVariation = IntegrationsSliceDefault;
+
+/**
+ * Integrations Shared Slice
+ *
+ * - **API ID**: `integrations`
+ * - **Description**: Integrations
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type IntegrationsSlice = prismic.SharedSlice<'integrations', IntegrationsSliceVariation>;
+
+/**
  * Primary content in *RichText → Primary*
  */
 export interface RichTextSliceDefaultPrimary {
@@ -798,6 +866,11 @@ declare module '@prismicio/client' {
 			HeroSliceDefaultPrimary,
 			HeroSliceVariation,
 			HeroSliceDefault,
+			IntegrationsSlice,
+			IntegrationsSliceDefaultPrimary,
+			IntegrationsSliceDefaultItem,
+			IntegrationsSliceVariation,
+			IntegrationsSliceDefault,
 			RichTextSlice,
 			RichTextSliceDefaultPrimary,
 			RichTextSliceVariation,
